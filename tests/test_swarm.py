0b1a4830807ce5a1409bca32a909bd70dc4104d9
Petr Belskiy <petr.belskiy@gmail.com>
2020-10-31 22:04:41 +0300

    move async client test to swarm module tests

--- tests/test_swarm.py
+++ tests/test_swarm.py
@@ -1,6 +1,17 @@
-import pytest
+import re
 
-from helixswarm import SwarmClient, SwarmError
+import pytest
+import responses
+
+from aioresponses import aioresponses
+
+from helixswarm import SwarmAsyncClient, SwarmClient, SwarmError
+
+GET_VERSION_DATA = {
+    'apiVersions': [1, 1.1, 1.2, 2, 3, 4, 5, 6, 7, 8, 9],
+    'version': 'SWARM/2018.2/1705499 (2018/09/25)',
+    'year': '2018'
+}
 
 
 def test_get_host_and_api_version():
@@ -20,3 +31,37 @@ def test_get_host_and_api_version():
     host, version = SwarmClient._get_host_and_api_version('swarm-server.com/api/v1.2')
     assert host == 'swarm-server.com'
     assert version == '1.2'
+
+
+@responses.activate
+def test_sync_client():
+    responses.add(
+        responses.GET,
+        re.compile(r'.*/api/v\d+/version'),
+        json=GET_VERSION_DATA,
+        status=200
+    )
+
+    try:
+        client = SwarmClient('http://server/api/v9', 'login', 'password')
+        version = client.get_version()
+        assert version['year'] == '2018'
+    finally:
+        client.close()
+
+
+@pytest.mark.asyncio
+async def test_async_client():
+    try:
+        client = SwarmAsyncClient('http://server/api/v9', 'login', 'password')
+        with aioresponses() as mock:
+            mock.get(
+                'http://server/api/v9/version',
+                payload=GET_VERSION_DATA,
+                status=200
+            )
+            version = await client.get_version()
+
+        assert version['year'] == '2018'
+    finally:
+        await client.close()
