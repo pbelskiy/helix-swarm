21e72dd8dc97b57b097b57d45f3d094295606677
Petr Belskiy <petr.belskiy@gmail.com>
2020-11-23 00:00:29 +0300

    add projects.edit()

--- helixswarm/endpoints/projects.py
+++ helixswarm/endpoints/projects.py
@@ -197,3 +197,148 @@ class Projects:
             data['minimumUpVotes'] = minimum_up_votes
 
         return self.swarm._request('POST', 'projects', data=data)
+
+    def edit(self,
+             identifier: str,
+             *,
+             name: str,
+             members: List[str],
+             subgroups: Optional[List[str]] = None,
+             owners: Optional[List[str]] = None,
+             description: Optional[str] = None,
+             is_private: Optional[bool] = None,
+             deploy_config: Optional[dict] = None,
+             tests_config: Optional[dict] = None,
+             branches: Optional[List[dict]] = None,
+             job_view: Optional[str] = None,
+             notify_commits: Optional[bool] = None,
+             notify_reviews: Optional[bool] = None,
+             defaults: Optional[List[dict]] = None,
+             retain_default_reviewers: Optional[bool] = None,
+             minimum_up_votes: Optional[str] = None
+             ) -> dict:
+        """
+        Edit a project.
+
+        * identifier: ``str``
+          Project ID
+
+        * name: ``str`` (optional)
+          Project name, changing the project name does not change the project ID.
+
+        * members: ``List[str]`` (optional)
+          Array of project members.
+
+        * subgroups: ``List[str]`` (optional)
+          Array of project subgroups.
+
+        * owners: ``List[str]`` (optional)
+          Array of project owners.
+
+        * description: ``str`` (optional)
+          Project description.
+
+        * private: ``bool`` (optional)
+          Private projects are visible only to: members, moderators, owners,
+          and administrators. Default: false
+
+        * deploy: ``dict`` (optional)
+          Configuration for automated deployment.
+
+          Example: {'enabled': True, 'url': 'http://localhost/?change={change}'}
+
+        * tests:  ``dict`` (optional)
+          Configuration for testing, continuous integration.
+
+          Example: {'url': '', 'enabled': False}
+
+        * branches: ``List[dict]`` (optional)
+          Branch definitions for this project.
+
+          Example:
+            [
+              {
+                'name': 'Branch One',
+                'paths': '//depot/main/TestProject/...'
+              }
+            ]
+
+        * jobview: ``str`` (optional)
+          Jobview for associating certain jobs with this project.
+
+          Example: 'subsystem=testproject'
+
+        * notify_commits: ``bool`` (optional)
+          Email members, moderators and followers when a change is committed.
+
+        * notify_reviews: ``bool`` (optional)
+          Email members and moderators when a new review is requested.
+
+        * defaults: ``List[dict]`` (optional)
+          Array of defaults at a project level (for example default reviewers).
+
+          Example: [{'reviewers': {'user2': {'required': True}}]
+
+        * retain_default_reviewers: ``bool`` (optional)
+          Retain the default reviewers.
+
+        * minimum_up_votes: ``str`` (optional)
+          Minimum number of up votes required.
+
+        :returns: ``dict``
+        :raises: ``SwarmError``
+        """
+        data = dict()  # type: Dict[str, Union[str, bool, dict, List[str], List[dict]]]
+
+        if name:
+            data['name'] = name
+
+        if members:
+            data['members'] = members
+
+        if subgroups:
+            data['subgroups'] = subgroups
+
+        if owners:
+            data['owners'] = owners
+
+        if description:
+            data['description'] = description
+
+        if is_private is not None:
+            data['private'] = is_private
+
+        if deploy_config:
+            data['deploy'] = deploy_config
+
+        if tests_config:
+            data['tests'] = tests_config
+
+        if branches:
+            data['branches'] = branches
+
+        if job_view:
+            data['jobview'] = job_view
+
+        if notify_commits is not None:
+            data['emailFlags[change_email_project_users]'] = notify_commits
+
+        if notify_reviews is not None:
+            data['emailFlags[review_email_project_members]'] = notify_reviews
+
+        if defaults:
+            data['defaults'] = defaults
+
+        if retain_default_reviewers is not None:
+            data['retainDefaultReviewers'] = retain_default_reviewers
+
+        if minimum_up_votes:
+            data['minimumUpVotes'] = minimum_up_votes
+
+        response = self.swarm._request(
+            'PATCH',
+            'projects/{}'.format(identifier),
+            data=data
+        )
+
+        return response
--- tests/test_projects.py
+++ tests/test_projects.py
@@ -118,3 +118,56 @@ def test_create():
     )
 
     assert 'project' in response
+
+
+@responses.activate
+def test_edit():
+    data = {
+        'project': {
+            'id': 'testproject3',
+            'defaults': [],
+            'branches': [{'name': 'Branch One', 'paths': '//depot/main/TestProject/...'}],
+            'deleted': False,
+            'deploy': {'url': '', 'enabled': False},
+            'description': 'The third iteration of our test project.',
+            'followers': [],
+            'jobview': 'subsystem=testproject',
+            'members': ['alice', 'bob'],
+            'name': 'TestProject 3',
+            'owners': ['root', 'admin'],
+            'private': False,
+            'minimumUpVotes': '2',
+            'retainDefaultReviewers': False,
+            'subgroups': ['subgroup'],
+            'tests': {'url': '', 'enabled': False}
+        }
+    }
+
+    responses.add(
+        responses.PATCH,
+        re.compile(r'.*/api/v\d+/projects/testproject3'),
+        json=data
+    )
+
+    client = SwarmClient('http://server/api/v2', 'login', 'password')
+
+    response = client.projects.edit(
+        'testproject3',
+        name='testproject3',
+        members=['alice', 'bob'],
+        subgroups=['subgroup'],
+        owners=['root', 'admin'],
+        description='The third iteration of our test project.',
+        is_private=False,
+        deploy_config={'url': '', 'enabled': False},
+        tests_config={'url': '', 'enabled': False},
+        branches=[{'name': 'Branch One', 'paths': '//depot/main/TestProject/...'}],
+        job_view='subsystem=testproject',
+        notify_commits=True,
+        notify_reviews=True,
+        defaults=[{'reviewers': {'user2': {'required': True}}}],
+        retain_default_reviewers=False,
+        minimum_up_votes='2'
+    )
+
+    assert 'project' in response
