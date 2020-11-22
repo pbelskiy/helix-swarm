1dae37625bbed4b32081fea1ddb1f76466680824
Petr Belskiy <petr.belskiy@gmail.com>
2020-11-22 11:45:26 +0300

    bump version to 0.5.0

--- helixswarm/__init__.py
+++ helixswarm/__init__.py
@@ -2,7 +2,7 @@ from .adapters.aio import SwarmAsyncClient
 from .adapters.sync import SwarmClient
 from .exceptions import SwarmCompatibleError, SwarmError, SwarmNotFoundError
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 __all__ = (
     # adapters
