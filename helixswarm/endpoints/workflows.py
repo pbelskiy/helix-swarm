0608a514c4caefd5094669aa78a9fafaf6f1c822
Petr Belskiy <petr.belskiy@gmail.com>
2020-11-24 00:03:24 +0300

    add workflows.get()

---
+++ helixswarm/endpoints/workflows.py
@@ -0,0 +1,40 @@
+from typing import Dict, List, Optional, Union
+
+from helixswarm.helpers import minimal_version
+
+
+class Workflows:
+
+    def __init__(self, swarm):
+        self.swarm = swarm
+
+    @minimal_version(9)
+    def get(self,
+            *,
+            fields: Optional[List[str]] = None,
+            no_cache: Optional[bool] = None
+            ) -> dict:
+        """
+        Gets workflows.
+
+        * fields:  ``List[str]`` (optional)
+          An optional list of fields to show for each workflow. Omitting this
+          parameter or passing an empty value shows all fields.
+
+        * noCache: ``bool`` (optional)
+          If provided and has a value of 'true' a query will always be performed
+          and the cache of workflows is ignored. Otherwise the cache will be used
+          if it exists.
+
+        :returns: ``dict``
+        :raises: ``SwarmError``
+        """
+        params = dict()  # type: Dict[str, Union[str, bool]]
+
+        if fields:
+            params['fields'] = ','.join(fields)
+
+        if no_cache:
+            params['noCache'] = no_cache
+
+        return self.swarm._request('GET', 'workflows', params=params)
---
+++ tests/test_workflows.py
@@ -0,0 +1,79 @@
+import re
+
+import responses
+
+from helixswarm import SwarmClient
+
+
+@responses.activate
+def test_get():
+    data = {
+        'workflows': [
+            {
+                'id': '1',
+                'name': 'myWorkflow',
+                'description': 'A description',
+                'on_submit': {
+                    'with_review': {
+                        'rule': 'no_checking'
+                    },
+                    'without_review': {
+                        'rule': 'no_checking'
+                    }
+                },
+                'auto_approve': {
+                    'rule': 'never'
+                },
+                'counted_votes': {
+                    'rule': 'anyone'
+                },
+                'shared': 'true',
+                'owners': [
+                    'user1',
+                    'user2'
+                ]
+            },
+            {
+                'id': '2',
+                'name': 'myWorkflow 2',
+                'description': 'A description',
+                'on_submit': {
+                    'with_review': {
+                        'rule': 'no_checking'
+                    },
+                    'without_review': {
+                        'rule': 'no_checking'
+                    }
+                },
+                'auto_approve': {
+                    'rule': 'votes'
+                },
+                'counted_votes': {
+                    'rule': 'members'
+                },
+                'shared': 'true',
+                'owners': [
+                    'user3',
+                    'user4'
+                ]
+            }
+        ]
+    }
+
+    responses.add(
+        responses.GET,
+        re.compile(r'.*/api/v\d+/workflows'),
+        json=data
+    )
+
+    client = SwarmClient('http://server/api/v9', 'login', 'password')
+
+    response = client.workflows.get(
+        fields=[
+            'id', 'name', 'description', 'on_submit', 'auto_approve',
+            'counted_votes', 'shared', 'owners'
+        ],
+        no_cache=True
+    )
+
+    assert 'workflows' in response
