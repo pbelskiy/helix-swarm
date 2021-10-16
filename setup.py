0abab4dec17f6c1cffe33691c31742ed29e00dc8
Petr Belskiy <petr.belskiy@gmail.com>
2021-10-16 17:20:26 +0300

    add Python 3.10

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     license='MIT',
     packages=find_packages(),
