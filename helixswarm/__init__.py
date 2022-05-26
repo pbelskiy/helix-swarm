bc70f1869cb0a29ed8bea536c1d50cf3f619a89f
Petr Belskiy <petr.belskiy@gmail.com>
2022-05-27 00:29:38 +0300

    bump to 0.7.2

--- helixswarm/__init__.py
+++ helixswarm/__init__.py
@@ -2,7 +2,7 @@ from .adapters.aio import SwarmAsyncClient
 from .adapters.sync import SwarmClient
 from .exceptions import SwarmCompatibleError, SwarmError, SwarmNotFoundError
 
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 
 __all__ = (
     # adapters
